9d47972066fd2a193be8e11860ad3d3486b7af86
Edoardo Rosa <edoardo.rosa90@gmail.com>
2016-05-11 12:26:31 +0200

    ledround

--- Arduino/src/JimmyChallenge.ino
+++ Arduino/src/JimmyChallenge.ino
@@ -2,29 +2,18 @@
 #include "SonarTask.h"
 #include "MsgService.h"
 #include "LedTask.h"
-#include "LedRoundTask.h"
+#include "LedRound.h"
 
 const int BAUD = 9600;
 const int MAX_DISTANCE_SONAR = 100;
 int channel[] = { 2, 3, 4, 5 };
 Scheduler sched;
+Context *c = new Context(0.5);
 
 void setup() {
     MsgService.init(BAUD, "JimmyChallenge");
+
     sched.init(100);
-    Context *c = new Context(0.5);
-
-    SonarTask *t0 = new SonarTask(12, 11, MAX_DISTANCE_SONAR, c);
-    t0->init(100);
-    sched.addTask(t0);
-
-    LedTask *t1 = new LedTask(13, c);
-    t1->init(10);
-    sched.addTask(t1);
-
-    LedRoundTask *t2 = new LedRoundTask(channel, 4, c);
-    t2->init(10);
-    sched.addTask(t2);
 }
 
 void loop() {
--- Arduino/src/LedRound.cpp
+++ Arduino/src/LedRound.cpp
@@ -1,33 +1,34 @@
 #include "LedRound.h"
 
-LedRound::LedRound(int *pins, int size) {
-    pins = pins;
-    size = size;
+LedRound::LedRound(int *p, int n) {
+    pins = p;
+    size = n;
     for (int pin = 0; pin < size; pin++) {
         pinMode(pins[pin], OUTPUT);
     }
 }
 
 void LedRound::switchOn() {
-    for (int i = 0; i < size; i++) {
-        digitalWrite(i, HIGH);
+    Serial.println(size);
+    for (int i = 0; i < size; ++i) {
+        digitalWrite(pins[i], HIGH);
     }
 }
 
-void LedRound::switchRoundOn(int *who, int size) {
-    for (int i = 0; i < size; i++) {
-        digitalWrite(pins[who[i]], HIGH);
+void LedRound::switchRoundOn(int *who, int n) {
+    for (int i = 0; i < n; ++i) {
+        digitalWrite(who[i], HIGH);
     }
 }
 
-void LedRound::switchRoundOff(int *who, int size) {
-    for (int i = 0; i < size; i++) {
+void LedRound::switchRoundOff(int *who, int n) {
+    for (int i = 0; i < n; ++i) {
         digitalWrite(pins[who[i]], LOW);
     }
 }
 
 void LedRound::switchOff() {
-    for (int i = 0; i < size; i++) {
-        digitalWrite(i, LOW);
+    for (int i = 0; i < size; ++i) {
+        digitalWrite(pins[i], LOW);
     }
 }
